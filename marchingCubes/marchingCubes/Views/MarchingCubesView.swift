5fb9dc0c30f693834e37a29174a55689236bd542
Mingxin Hou <mingxinh@uci.edu>
2024-10-24 17:17:08 -0700

    asyncly convert 3d models

--- marchingCubes/marchingCubes/Views/MarchingCubesView.swift
+++ marchingCubes/marchingCubes/Views/MarchingCubesView.swift
@@ -2,90 +2,88 @@ import SwiftUI
 import SceneKit
 
 struct MarchingCubesView: View {
-    let filename = "rabbit" // Store the filename here
-    let divisions = 5 // Define divisions as a variable
+    let filename = "rabbit"
+    let divisions = 5
+
+    @State private var isLoading = true
 
     var body: some View {
-        ScrollView {
-            VStack {
-                Text("\(filename.capitalized)") // Use the filename as the title
-                    .font(.largeTitle)
-                    .padding()
+        ZStack {
+            ScrollView {
+                VStack {
+                    Text("\(filename.capitalized)")
+                        .font(.largeTitle)
+                        .padding()
 
-                SceneView(filename: filename, divisions: divisions) // Pass divisions
-                    .frame(width: 300, height: 300) // Set fixed size
-                    .edgesIgnoringSafeArea(.all)
-                
-//                SceneView(filename: filename, divisions: 5) // Pass divisions
-//                    .frame(width: 300, height: 300) // Set fixed size
-//                    .edgesIgnoringSafeArea(.all)
-//                
-//                SceneView(filename: filename, divisions: 4) // Pass divisions
-//                    .frame(width: 300, height: 300) // Set fixed size
-//                    .edgesIgnoringSafeArea(.all)
-//                
-//                SceneView(filename: filename, divisions: 3) // Pass divisions
-//                    .frame(width: 300, height: 300) // Set fixed size
-//                    .edgesIgnoringSafeArea(.all)
+                    SceneView(filename: filename, divisions: divisions, isLoading: $isLoading)
+                        .frame(width: 300, height: 300)
+                        .edgesIgnoringSafeArea(.all)
+                }
             }
-        }
-    }
-}
 
-struct MarchingCubes_Previews: PreviewProvider {
-    static var previews: some View {
-        MarchingCubesView()
+            LoadingView(filename: filename, isLoading: isLoading)
+        }
     }
 }
 
 struct SceneView: UIViewRepresentable {
-    let filename: String // Receive the filename
-    let divisions: Int // Receive divisions
+    let filename: String
+    let divisions: Int
+    @Binding var isLoading: Bool
 
     func makeUIView(context: Context) -> SCNView {
-        guard let obj = loadOBJ(filename: filename),
-              let voxarr = voxelize(asset: obj, divisions: Int32(divisions)) else {
-            print("Failed to load or voxelize the model.")
-            return SCNView()
-        }
-        
         let scnView = SCNView()
         scnView.allowsCameraControl = true
-//        scnView.showsStatistics = true
         scnView.backgroundColor = UIColor(red: 0.0, green: 0.4, blue: 0.65, alpha: 1.0) // UCI Blue
-        
+
         let scene = SCNScene()
-
-        let voxelGrid = convertTo3DArray(voxelArray: voxarr)
-        let mcNode = marchingCubes(data: voxelGrid)
-//        let mcNode = marchingCubesSingleLayer(data: voxelGrid, layer: 40)
-        
-        // Make the object shiny
-        let material = SCNMaterial()
-        material.diffuse.contents = UIColor.white
-        material.specular.contents = UIColor.white
-        material.shininess = 30 // Adjust shininess for more shine
-        mcNode.geometry?.firstMaterial = material
-        
-        // Create outline by duplicating the node
-        let outlineNode = mcNode.clone()
-        outlineNode.geometry = mcNode.geometry?.copy() as? SCNGeometry
-        outlineNode.geometry?.firstMaterial = SCNMaterial()
-        outlineNode.geometry?.firstMaterial?.diffuse.contents = UIColor.black
-        outlineNode.geometry?.firstMaterial?.fillMode = .lines
-
-        scene.rootNode.addChildNode(mcNode)
-        scene.rootNode.addChildNode(outlineNode)
-        
-        // Add lights to the scene
-        addLights(to: scene)
-    
         scnView.scene = scene
+
+        Task {
+            await loadAndProcessModel(in: scene, scnView: scnView)
+        }
+
         return scnView
     }
-    
+
     func updateUIView(_ uiView: SCNView, context: Context) {}
-    
+
+    private func loadAndProcessModel(in scene: SCNScene, scnView: SCNView) async {
+        DispatchQueue.main.async {
+            isLoading = true
+        }
+
+        let result: (SCNNode?, SCNNode?) = await Task {
+            guard let obj = loadOBJ(filename: filename),
+                  let voxarr = voxelize(asset: obj, divisions: Int32(divisions)) else {
+                print("Failed to load or voxelize the model.")
+                return (nil, nil)
+            }
+
+            let voxelGrid = convertTo3DArray(voxelArray: voxarr)
+            let mcNode = marchingCubes(data: voxelGrid)
+
+            // Create outline by duplicating the node
+            let outlineNode = mcNode.clone()
+            outlineNode.geometry = mcNode.geometry?.copy() as? SCNGeometry
+            outlineNode.geometry?.firstMaterial = SCNMaterial()
+            outlineNode.geometry?.firstMaterial?.diffuse.contents = UIColor.black
+            outlineNode.geometry?.firstMaterial?.fillMode = .lines
+
+            return (mcNode, outlineNode)
+        }.value
+
+        // Update UI on the main thread
+        DispatchQueue.main.async {
+            if let mcNode = result.0, let outlineNode = result.1 {
+                scene.rootNode.addChildNode(mcNode)
+                scene.rootNode.addChildNode(outlineNode)
+                addLights(to: scene)
+            }
+            isLoading = false
+        }
+    }
+
     private func addLights(to scene: SCNScene) {
         let keyLightNode1 = SCNNode()
         let keyLight1 = SCNLight()
@@ -109,29 +107,35 @@ struct SceneView: UIViewRepresentable {
         keyLightNode2.eulerAngles = SCNVector3(Float.pi / 4, Float.pi / 4, 0)
         scene.rootNode.addChildNode(keyLightNode2)
         
-//        let keyLightNode3 = SCNNode()
-//        let keyLight3 = SCNLight()
-//        keyLight3.type = .directional
-//        keyLight3.intensity = 100
-//        keyLight3.castsShadow = false
-//        keyLightNode3.light = keyLight3
-//        keyLightNode3.eulerAngles = SCNVector3(-Float.pi / 4, Float.pi / 4, 0)
-//        scene.rootNode.addChildNode(keyLightNode3)
-//        
-//        let keyLightNode4 = SCNNode()
-//        let keyLight4 = SCNLight()
-//        keyLight4.type = .directional
-//        keyLight4.intensity = 100
-//        keyLight4.castsShadow = false
-//        keyLightNode4.light = keyLight4
-//        keyLightNode4.eulerAngles = SCNVector3(Float.pi / 4, -Float.pi / 4, 0)
-//        scene.rootNode.addChildNode(keyLightNode4)
-        
         let fillLightNode = SCNNode()
         let fillLight = SCNLight()
         fillLight.type = .ambient
         fillLight.intensity = 600
         fillLightNode.light = fillLight
         scene.rootNode.addChildNode(fillLightNode)
+    }
+}
+
+struct LoadingView: View {
+    let filename: String
+    let isLoading: Bool
+
+    var body: some View {
+        if isLoading {
+            Text("Converting \(filename) to cubes...")
+                .font(.headline)
+                .foregroundColor(.white)
+                .padding()
+                .background(Color.black.opacity(0.7))
+                .cornerRadius(10)
+                .transition(.opacity)
+                .animation(.easeInOut, value: isLoading)
+        }
+    }
+}
+
+struct MarchingCubes_Previews: PreviewProvider {
+    static var previews: some View {
+        MarchingCubesView()
     }
 }
