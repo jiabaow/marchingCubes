005b75154644ad09341906b81c0fed36b25d01f7
jiabaow <jiabaowenwendy@gmail.com>
2024-11-13 16:20:27 -0800

    put voxel data loader in view model

--- marchingCubes/marchingCubes/Views/MarchingCubesView.swift
+++ marchingCubes/marchingCubes/Views/MarchingCubesView.swift
@@ -164,42 +164,6 @@ struct LoadingView: View {
     }
 }
 
-class VoxelDataLoader: ObservableObject {
-    @Published var voxelData: [[[Int]]] = []
-    @Published var numLayer: Int = 0
-    @Published var isLoading: Bool = true
-    @Published var scnNodesByLayer: [Int: [SCNNode?]] = [:]
-
-    func loadVoxelData(filename: String, divisions: Int) {
-        DispatchQueue.global(qos: .userInitiated).async {
-            guard let (loadedVoxelData, loadedNumLayer) = MarchingCubesView.loadVoxelData(filename: filename, divisions: divisions) else {
-                print("Failed to load voxel data.")
-                DispatchQueue.main.async {
-                    self.isLoading = false
-                }
-                return
-            }
-
-            DispatchQueue.main.async {
-                self.voxelData = loadedVoxelData
-                self.numLayer = loadedNumLayer
-                self.isLoading = false
-                self.loadSCNNodesForAllLayers()
-            }
-        }
-    }
-
-    private func loadSCNNodesForAllLayers() {
-        for layer in 0...numLayer {
-            let isTopLayer = (layer == numLayer)
-            let nodes = MarchingCubesView.loadSCNNodesByLayer(numLayer: layer + 1, voxelData: voxelData, isTopLayer: isTopLayer)
-            scnNodesByLayer[layer] = nodes
-        }
-        let fullNode = MarchingCubesView.loadSCNNodesByLayer(numLayer: numLayer + 1, voxelData: voxelData, isTopLayer: false)
-        scnNodesByLayer[0] = fullNode
-    }
-}
-
 struct MarchingCubes_Previews: PreviewProvider {
     static var previews: some View {
         MarchingCubesView()
